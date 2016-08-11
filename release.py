47d2d18ee26e5fdb94a5c84aca5ebf813d67939b
Zachary Yee <Zachary.Yee@teradata.com>
2016-08-11 15:55:14 -0400

    use zach repo

--- release.py
+++ release.py
@@ -27,7 +27,7 @@ try:
 except ImportError:
     from distutils.core import Command
 
-GITHUB_REPOSITORY_API_PATH = 'https://api.github.com/repos/prestodb/presto-admin'
+GITHUB_REPOSITORY_API = 'https://api.github.com/repos/zachyee/presto-admin'
 CURRENT_DIRECTORY = os.path.dirname(os.path.realpath(__file__))
 
 
